a6faf3fe450eabae7fb1ed36f2a841e84c3ec574
Amit P <aspper20@gmail.com>
2022-01-25 03:33:42 +0530

    pagination tested, build folder removed

--- client/build/js/chunk-2d0e9722.6bd84f28.js
+++
@@ -1,6 +0,0 @@
-(window.webpackJsonp = window.webpackJsonp || []).push([['chunk-2d0e9722'], {
-  '8e21': function (e, t, n) {
-    n.r(t); const i = function () { const e = this; const t = e.$createElement; e._self._c; return e._m(0); }; const a = [function () { const e = this; const t = e.$createElement; const n = e._self._c || t; return n('div', { staticClass: 'bg-white shadow-sm rounded-md' }, [n('p', [e._v('Notifications Homepage')])]); }]; const o = { name: 'NotificationHome' }; const s = o; const c = n('2877'); const r = Object(c.a)(s, i, a, !1, null, null, null); t.default = r.exports;
-  },
-}]);
-// # sourceMappingURL=chunk-2d0e9722.6bd84f28.js.map
--- client/src/views/borrow/home.vue
+++ client/src/views/borrow/home.vue
@@ -185,7 +185,7 @@
                 <div class="class max-w-2xl">
                   <t-pagination
                     v-model="urlParams.page"
-                    :total-items="allBorrowings.length"
+                    :total-items="borrowCount"
                     :per-page="urlParams.limit"
                     :limit="5"
                   />
@@ -229,17 +229,27 @@ export default {
       deleteMessage: '',
       urlParams: {
         page: 1,
-        limit: 20,
+        limit: 5,
       },
     };
   },
   computed: {
     ...mapGetters({
       allBorrowings: borrowTypes.GET_ALL_BORROWINGS,
+      borrowCount: borrowTypes.GET_BORROW_COUNT
     }),
   },
+  watch: {
+    $route() {
+      this.getAllBorrowings(this.urlParams);
+    },
+    urlParams: {
+      handler: 'updateRoute',
+      deep: true,
+    },
+  },
   mounted() {
-    this.getAllBorrowings();
+    this.getAllBorrowings(this.urlParams);
   },
   methods: {
     ...mapActions({
@@ -248,6 +258,13 @@ export default {
       deleteBorrowAction: borrowTypes.DELETE_BORROW_ACTION,
       getAllBorrowings: borrowTypes.GET_ALL_BORROWINGS_ACTION,
     }),
+    async updateRoute() {
+      try {
+        await this.$router.push({ name: 'BorrowHome', query: this.urlParams });
+      } catch (navigationError) {
+        // Catch and ignore navigation errors caused through multiple params changed
+      }
+    },
     addBorrow(payload) {
       const formattedPayload = {
         borrowedOn: payload.borrowedOn,
